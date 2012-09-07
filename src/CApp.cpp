4e3025c0e92b1261d7519c6019ac93f8b5ce72c3
Tanner Danzey <arkaniad@gmail.com>
2012-09-06 21:49:07 -0500

    First commit. Whee!

---
+++ src/CApp.cpp
@@ -0,0 +1,27 @@
+#include "CApp.h"
+
+CApp::CApp() {
+}
+
+int CApp::OnExecute() {
+  if(OnInit() == false) {
+    return -1;
+  }
+  SDL_Event Event;
+
+  while(Running) {
+    while(SDL_PollEvent(&Event)) {
+      OnEvent(&Event);
+    }
+    
+    OnLoop();
+    OnRender();
+  }
+  OnCleanup();
+  return 0;
+}
+
+int main(int argc, const char *argv[]) {
+  CApp theApp;
+  return theApp.OnExecute();
+}
